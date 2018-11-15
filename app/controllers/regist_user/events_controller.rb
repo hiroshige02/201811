423303f917269ee0015df472c72b15bf23bd5e5b
Hiroshige02 <oajeilwi00@gmail.com>
2018-11-15 07:23:20 +0000

    11/15 時間比較式完成

--- app/controllers/regist_user/events_controller.rb
+++ app/controllers/regist_user/events_controller.rb
@@ -38,7 +38,7 @@ class RegistUser::EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.regist_user_id = current_regist_user.id
-    a = params[:a]
+    a = params[:a].to_i
     b = params[:b]
     c = params[:c]
     d = params[:d]
@@ -46,95 +46,100 @@ class RegistUser::EventsController < ApplicationController
     h = Time.now
     j = h.month
 
-    if j == 12 && a == 1
-      i = h.year + 1
+
       # binding.pry
-      elsif j == 11 || j == 12
+      if j == 11 || j == 12
         if a == 1 || a == 2
-        # binding.pry
-        i = h.year + 1
-        # binding.pry
-        else i = h.year
-        # binding.pry
+          i = h.year + 1
+        else
+          i = h.year
         end
-      else i = h.year
-        # binding.pry
-    end
-
-     binding.pry
-    @event.start_time = Time.mktime(i, a, b, c, 0, 0)
-    @event.finish_time = Time.mktime(i, a, b, d, 0, 0)
+      else
+        i = h.year
+      end
 
 
 
-    # if @event.start_time > h || @event.finish_time > @event.start_time
-  # ここまでで予約時間内での比較は終了。この中で他予約との比較演算を記載。
-  #     puts "true"
-  #     binding.pry
-  # #もし同じ日付のものがあったら
-  #     event_exists = Event.all
-  #     binding.pry
+    x = Time.mktime(i, a, b, c, 0, 0)
+    y = Time.mktime(i, a, b, d, 0, 0)
 
-
-  #       event_exists.each do |event_exist|
-  #         if event_exist.start_time.to_date == @event.start_time.to_date
-  #           puts "bbb"
-  #           #同じ日付の時はこちらに流れるのでここから時間の比較演算を行う
-  #           if event_exist.start_time >= @event.start_time && event_exist.start_time < @event.finish_time
-  #             puts "kkk"
-  #             binding.pry
-  #           end
-
-  #           if event_exist.start_time <= @event.start_time && event_exist.finish_time >= @event.finish_time
-  #             puts "lll"
-  #             binding.pry
-  #           end
-
-  #           if event_exist.finish_time > @event.start_time && event_exist.finish_time <= @event.finish_time
-  #             puts "mmm"
-  #             binding.pry
-  #           end
-
-  #           if event_exist.start_time > @event.start_time && event_exist.finish_time < @event.finish_time
-  #             puts "nnn"
-  #             binding.pry
-  #           end
+    binding.pry
 
 
 
-          # else
+    if x > h && y > x
+  #ここまでで予約時間内での比較は終了。この中で他予約との比較演算を記載。
+    if Event.exists?
+        #↑ Eventレコードが存在しない時エラーが出るため補正
+      event_exists = Event.all
+      binding.pry
 
-            #同じ日付のものがないのでOKの場合このelseに流れる。この場合save続行
-            if @event.save
-              redirect_to regist_user_path(current_regist_user.id)
-              binding.pry
-            else render:new
+        event_exists.each do |event_exist|
+          if event_exist.start_time.to_date == x.to_date
+            puts "bbb"
+            #同じ日付の時はこちらに流れるのでここから時間の比較演算を行う
+            if event_exist.start_time >= x && event_exist.start_time < y
+              puts "kkk"
+              flash[:alert] = "予約時間が重複しています"
+              render :new and return
               binding.pry
             end
+
+            if event_exist.start_time < x && event_exist.finish_time >= y
+              puts "lll"
+              flash[:alert] = "予約時間が重複しています"
+              render :new and return
+              binding.pry
+            end
+
+            if event_exist.finish_time > x && event_exist.finish_time <= y
+              puts "mmm"
+              flash[:alert] = "予約時間が重複しています"
+              render :new and return
+              binding.pry
+            end
+
+            if event_exist.start_time > x && event_exist.finish_time < y
+              puts "nnn"
+              flash[:alert] = "予約時間が重複しています"
+              render :new and return
+              binding.pry
+            end
+
+          else
+            binding.pry
+            @event.start_time = x
+            @event.finish_time = y
+            if @event.save
+            redirect_to regist_user_path(current_regist_user.id) and return
+            else render:new
+            binding.pry
+            end
           end
-        # end #eachdoのend
+        end #doのend
 
-  #     else
-  #       puts "設定時間を確認してください"
-  #     end
+    else
+        #Eventレコードが1つも存在しないときの処理
+        binding.pry
+        @event.start_time = x
+        @event.finish_time = y
+        if @event.save
+        binding.pry
+        redirect_to regist_user_path(current_regist_user.id) and return
+        else render:new
+        binding.pry
+      end
+    end
 
-  # end
+      else
+        #時間設定がそもそもおかしい
+        flash[:alert] = "設定日時を確認してください"
+        render :new and return
+      end
+
+   end
 
 
-
-
-
-    # respond_to do |format|
-    #     format.html { redirect_to @event, notice: 'Event was successfully created.' }
-    #     format.json { render :show, status: :created, location: @event }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @event.errors, status: :unprocessable_entity }
-    #   end
-    # end
-
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -159,12 +164,12 @@ class RegistUser::EventsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
+    # def set_event
+    #   @event = Event.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :content, :admin_ok, :admin_message, :admin_no, :participant, :event_image)
+      params.require(:event).permit(:title, :content, :admin_ok, :admin_message, :admin_no, :participant, :event_image, :desk)
     end
 end
